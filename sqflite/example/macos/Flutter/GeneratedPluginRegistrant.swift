864f5320609de85190b1ca4a24b9f5e50017595c
David Martos <davidmartos96@gmail.com>
2021-09-30 19:15:52 +0200

    generated file

---
+++ sqflite/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import sqflite
+import sqflite_sqlcipher
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  SqfliteSqlCipherPlugin.register(with: registry.registrar(forPlugin: "SqfliteSqlCipherPlugin"))
+}
